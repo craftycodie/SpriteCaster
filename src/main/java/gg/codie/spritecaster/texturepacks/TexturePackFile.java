d971b2a68dbeec0a1b4caa7dc35edd8d494a2622
Codie Stella <git@codie.gg>
2021-09-18 17:07:47 +0100

    Corrected portal mcmeta.

--- src/main/java/gg/codie/spritecaster/texturepacks/TexturePackFile.java
+++ src/main/java/gg/codie/spritecaster/texturepacks/TexturePackFile.java
@@ -8,7 +8,7 @@ public enum TexturePackFile {
     LAVA_FLOWING_MCMETA("custom_lava_flowing.mcmeta"),
     FIRE_0("custom_fire_n_s.mcmeta"),
     FIRE_1("custom_fire_e_w.mcmeta"),
-    PORTAL_MCMETA("custom_portal_mcmeta"),
+    PORTAL_MCMETA("custom_portal.mcmeta"),
     GEAR_CLOCKWISE_MCMETA("custom_gear_clockwise.mcmeta"),
     GEAR_COUNTER_CLOCKWISE_MCMETA("custom_gear_counter_clockwise.mcmeta"),
     SPRITECASTER_INFO("spritecaster.txt");
